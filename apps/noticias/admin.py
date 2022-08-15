30e0ba07ba6a5b60339e04920a6bedf269d919c7
Carlos A. Gomez <gomezcarlos03051@gmail.com>
2022-08-15 15:36:38 -0300

    Actualizo quienes somos.html, main.css y models.py

--- apps/noticias/admin.py
+++ apps/noticias/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models  import Noticia, Categoria
+from .models  import Comentario, Noticia, Categoria
 
 admin.site.register(Noticia)
 admin.site.register(Categoria)
+admin.site.register(Comentario)
