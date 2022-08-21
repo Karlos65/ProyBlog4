ce816e448162e308ebdfc2c61c36d2031be0651e
AlfreVel <88112763+AlfreVel@users.noreply.github.com>
2022-08-21 16:00:10 -0300

    Agregado filtrado por fecha

--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Noticia, Comentario, Categoria
 # Create your views here.
+
+#Meses
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre',
+         'Noviembre', 'Diciembre']
+
 def Listar(request):
     #Creo el diccionario para pasar los datos al templates
     ctx = {}
@@ -8,19 +13,32 @@ def Listar(request):
     todas = noticias = Noticia.objects.all()
 
     # Filtrar por categoria
-    if ("categoria" in request.GET):
+    if "categoria" in request.GET:
         categoria = request.GET['categoria']
         noticias = noticias.filter(categoria=categoria)
 
-    # Filtrar pro autor
-    if ("autor" in request.GET):
+    # Filtrar por autor
+    if "autor" in request.GET:
         autor = request.GET['autor']
         noticias = noticias.filter(autor=autor)
+
+    # Filtrar por Anio
+    if "anio" in request.GET:
+        anio = request.GET['anio']
+        noticias = noticias.filter(creado__year=anio)
+
+    # Filtrar por mes
+    if "mes" in request.GET:
+        mes = request.GET['mes']
+        if mes != "0":
+            noticias = noticias.filter(creado__month=mes)
 
     #Pasarlo al template
     ctx['notis'] = noticias
     ctx['categorias'] = Categoria.objects.all()
     ctx['autores'] = todas.values_list('autor', flat=True).distinct
+    ctx['anios'] = todas.values_list('creado__year', flat=True).distinct
+    ctx['meses'] = meses
 
     return render(request,'noticias/listar_noticias.html',ctx)
 
