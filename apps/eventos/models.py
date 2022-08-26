83d5cf3f4c18b1621bb20529c888aea906cd584b
Carlos A. Gomez <gomezcarlos03051@gmail.com>
2022-08-26 11:56:21 -0300

    Se agrega models de los Eventos y html para mostrar

--- apps/eventos/models.py
+++ apps/eventos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=120)
+    creado = models.DateField(auto_now_add=True)
+    cuerpo = models.TextField()
+    imagen = models.ImageField(upload_to = 'eventos',null=True, blank=True)
+        
+    def __str__(self):
+        return self.titulo
--- apps/eventos/views.py
+++ apps/eventos/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Evento
 # Create your views here.
 def Evento(request):
-    return render(request, 'eventos/listar_eventos.html')
+    #Creo el diccionario para pasar los datos al templates
+    ctx = {}
+    #Buscar lo que quiero de la BD
+    todas = Evento.objects.all()
+    
+    #Pasarlo al template
+    ctx['even'] = todas
+    
+    return render(request, 'eventos/listar_eventos.html',ctx)
