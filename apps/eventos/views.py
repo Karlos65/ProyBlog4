33a618c989688cc98f0fa6ee83b5fb7fa131d71a
Carlos A. Gomez <gomezcarlos03051@gmail.com>
2022-08-29 11:14:22 -0300

    Se agrega filtrado de eventos. Se agrega contenido pag. Home. Se agrega logo

--- apps/eventos/views.py
+++ apps/eventos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Evento
+from .models import Evento, Categoria
 
 # Create your views here.
 def Evento1(request):
@@ -8,8 +8,13 @@ def Evento1(request):
     ctx = {}
     #Buscar lo que quiero de la BD
     todos = Evento.objects.all()
+    # Filtrar por categoria
+    if "categoria" in request.GET:
+        categoria = request.GET['categoria']
+        if categoria != "0":
+            todos = todos.filter(categoria=categoria)
     
     #Pasarlo al template
     ctx['even'] = todos
-    
+    ctx['categorias'] = Categoria.objects.all()
     return render(request,'eventos/listar_eventos.html',ctx)
