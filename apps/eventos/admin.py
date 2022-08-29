1f7fc1e76a7eec4963d6039ae67bab4cf408e116
Carlos A. Gomez <gomezcarlos03051@gmail.com>
2022-08-28 22:52:41 -0300

    Se modifica BD - Eventos. Segrega: Categoria y Modalidad

--- apps/eventos/admin.py
+++ apps/eventos/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models  import Evento
+from .models  import Evento, Categoria, Modalidad
 # Register your models here.
 admin.site.register(Evento)
+admin.site.register(Categoria)
+admin.site.register(Modalidad)
--- apps/eventos/models.py
+++ apps/eventos/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=60)
+    decripcion = models.CharField(max_length=250, null=True, blank=True)
+    def __str__(self):
+        return self.nombre
+
+class Modalidad(models.Model):
+    nombre = models.CharField(max_length=60)
+    def __str__(self):
+        return self.nombre
+    
 class Evento(models.Model):
     titulo = models.CharField(max_length=120)
     creado = models.DateField(auto_now_add=True)
     lugar = models.CharField(max_length=120)
     cuerpo = models.TextField()
     imagen = models.ImageField(upload_to = 'eventos',null=True, blank=True)
-        
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null = True)
+    modalidad = models.ForeignKey(Modalidad, on_delete=models.CASCADE, null = True)
+    
     def __str__(self):
         return self.titulo
+    
+    
